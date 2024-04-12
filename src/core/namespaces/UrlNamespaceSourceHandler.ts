c884f4296e3177b226e812ba22b22e4762f9e8af
Stefan Schramm <mail@stefanschramm.net>
2024-04-12 07:43:32 +0200

    Implement UrlNamespaceSourceHandler

--- src/core/namespaces/UrlNamespaceSourceHandler.ts
+++ src/core/namespaces/UrlNamespaceSourceHandler.ts
@@ -1,13 +1,42 @@
-import { NamespaceSource, ShortcutSearchKeyMap } from '../Environment';
-import { NamespaceSourceHandler } from './NamespaceDispatcher';
+import { DataDefinitionError, ImplementationError, UsageError } from '../../Error.js';
+import { NamespaceSource, ShortcutSearchKeyMap } from '../Environment.js';
+import { NamespaceSourceHandler } from './NamespaceDispatcher.js';
+import yaml from 'yaml';
 
 export class UrlNamespaceSourceHandler implements NamespaceSourceHandler {
+  private cache: Record<string, ShortcutSearchKeyMap> = {};
+
   public supports(source: NamespaceSource): boolean {
     return typeof source === 'object' && source.url !== undefined;
   }
 
   public async get(source: NamespaceSource): Promise<ShortcutSearchKeyMap> {
-    console.log('getting source:', source);
-    throw new Error('TODO Method not implemented.'); // TODO
+    if (!(typeof source === 'object' && source.url !== undefined)) {
+      throw new ImplementationError('NamespaceSource not supported by UrlNamespaceSourceHandler.');
+    }
+
+    if (this.cache[source.url] === undefined) {
+      await this.load(source.url);
+    }
+
+    return this.cache[source.url];
+  }
+
+  private async load(url: string): Promise<void> {
+    try {
+      const response = await fetch(url);
+      const namespaceData = yaml.parse(await response.text());
+      this.cache[url] = namespaceData;
+    } catch (e) {
+      if (e instanceof yaml.YAMLParseError) {
+        throw new DataDefinitionError(`Unable to parse data for URL "${url}": {e.message}`);
+      } else {
+        const message =
+          e instanceof Error
+            ? `Unable to load data for for URL "${url}": ${e.message}`
+            : `Unable to load data for for URL "${url}.`;
+        throw new UsageError(message);
+      }
+    }
   }
 }
